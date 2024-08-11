5206c67a5682cd4a244132a9e3be75c2b96db590
Akshat2912 <149098611+Akshat2912@users.noreply.github.com>
2024-08-11 12:15:13 +0530

    Add files via upload

---
+++ q5.py
@@ -0,0 +1,9 @@
+int int_var;
+double double_var;
+std::string string_var;
+
+ros::param::get("/my_integer", int_var);
+ros::param::get("/my_float", double_var);
+ros::param::get("/my_string", string_var);
+
+ROS_INFO("Int: %d, Float: %lf, String: %s", int_var, double_var, string_var.c_str());
