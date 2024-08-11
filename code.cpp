dbd98985e16570c6eae684d3db15485481580ebb
Akshat2912 <149098611+Akshat2912@users.noreply.github.com>
2024-08-11 12:09:15 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+class PWM{
+    PWM(){
+        int motr;
+        int dia;
+        std::cin>>motr
+        std::cin>>dia
+    }
+    int func(int sp){
+        int pwm=sp*100/255
+        return pwm
+        
+    }
+}
